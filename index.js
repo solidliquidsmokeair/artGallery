5643c8dc6351308f484ab828135089f9b5e3f9dc
solidliquidsmokeair <sjgogan@icloud.com>
2021-12-02 17:13:23 +1300

    initial setup and building first object

---
+++ index.js
@@ -0,0 +1,17 @@
+const images = [
+    {
+    id: 1,
+    name: 'yin and yang',
+    image: '/images/yinYang.jpg'
+},
+{
+    id: 2,
+    name: 'mobius strip',
+    image: '/images/mobiusStrip.jpg'
+},
+{
+    id: 2,
+    name: 'refracted light',
+    image: '/images/refractedLight.jpg'
+},
+]
