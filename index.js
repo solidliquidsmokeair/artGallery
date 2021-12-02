8ac43eb963579debc392391d98d52de461611c83
solidliquidsmokeair <sjgogan@icloud.com>
2021-12-02 17:54:16 +1300

    add initial image on page load

--- index.js
+++ index.js
@@ -3,18 +3,39 @@ const images = [
     {
     id: 1,
     name: 'yin and yang',
-    image: '/images/yinYang.jpg'
+    image: 'images/yinYang.jpg'
 },
 {
     id: 2,
     name: 'mobius strip',
-    image: '/images/mobiusStrip.jpg'
+    image: 'images/mobiusStrip.jpg'
 },
 {
     id: 2,
     name: 'refracted light',
-    image: '/images/refractedLight.jpg'
+    image: 'images/refractedLight.jpg'
 },
 ]
 
-const nextBtn = document.querySelector('nextButton')
+const activeImage = document.querySelector('#picture')
+const imageName = document.querySelector("#imageName")
+
+const nextBtn = document.querySelector('#nextButton')
+const prevBtn = document.querySelector('#prevButton')
+
+
+let show = 1
+
+//load initial item
+
+window.addEventListener('DOMContentLoaded', function(){
+    var item = images[show]
+    imageName.innerText = item.name
+    activeImage.src = item.image
+
+})
+
+nextBtn.addEventListener("click", function(){
+    show = 1
+})
+
