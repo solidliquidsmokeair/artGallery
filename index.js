57fe3d3b8f024e3be591b94169f284ec4d0690dc
solidliquidsmokeair <sjgogan@icloud.com>
2021-12-02 23:00:56 +1300

    added more images, button functionlaity

--- index.js
+++ index.js
@@ -2,50 +2,78 @@
 const images = [
     {
     id: 1,
-    name: 'yin and yang',
-    image: 'images/yinYang.jpg'
+    title: 'yin and yang',
+    img: 'images/yinYang.jpg'
 },
 {
     id: 2,
-    name: 'mobius strip',
-    image: 'images/mobiusStrip.jpg'
+    title: 'mobius strip',
+    img: 'images/mobiusStrip.jpg'
 },
 {
     id: 3,
-    name: 'refracted light',
-    image: 'images/refractedLight.png'
+    title: 'refracted light',
+    img: 'images/refractedLight.png'
+},
+{
+    id: 4,
+    title: 'ordo ab chao',
+    img: 'images/ordoAbChao.gif'
+},
+{
+    id: 5,
+    title: 'dr strangelove',
+    img: 'images/FOaB.gif'
+},
+{
+    id: 6,
+    title: 'EARTH!',
+    img: 'images/blueMarble.jpg'
 },
 ]
 
 const activeImage = document.querySelector('#picture')
 const imageName = document.querySelector("#imageName")
 
-const nextBtn = document.querySelector('#nextButton')
-const prevBtn = document.querySelector('#prevButton')
+const nextBtn = document.querySelector('.nextOne')
+const prevBtn = document.querySelector('.lastOne')
 
 
-let show = 2
+let show = 0
 
 //load initial item
 
 window.addEventListener('DOMContentLoaded', function(){
-    showPerson(show)
+    showPerson()
 
 })
 
 //show person based on item
 
-function showPerson(person){
-    const item = images[person]
-    imageName.innerText = item.name
-    activeImage.src = item.image
+function showPerson(){
+    // var item = images[person]
+    imageName.innerText = images[show].title
+    activeImage.src = images[show].img
+    // console.log(item.img)
 }
 
 //prev and next buttons
 
 nextBtn.addEventListener('click', function(){
-    console.log('yopyo')
-    show++
+    if (show == 5){
+        show = 5
+    } else {
+        show ++
+    }
     showPerson()
 })
 
+prevBtn.addEventListener('click', function(){
+    if (show == 0){
+        show == 0
+    } else {
+        show --
+    }
+
+    showPerson()
+})
